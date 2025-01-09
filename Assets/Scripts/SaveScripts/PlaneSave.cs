87fc5f79edb28982c1af6f746934efd61dbf4a1d
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2025-01-09 09:25:28 +0400

    Переход к сохранению

--- Assets/Scripts/SaveScripts/PlaneSave.cs
+++ Assets/Scripts/SaveScripts/PlaneSave.cs
@@ -6,12 +6,17 @@ public class SaveTest : MonoBehaviour, IInteractable
 {
     private SaveController _controller;
     [SerializeField] private PlayableDirector _playableDirector;
+    [Header("Сохранение")]
+    [SerializeField] private MeshRenderer _meshRenderer;
+    private Texture2D _meshTexture;
 
     [Inject]
     public void Construct(SaveController controller)
     {
         Debug.Log("Все окк");
         _controller = controller;
+        _meshTexture = new Texture2D(100, 100);
+        _meshRenderer.material.mainTexture = _meshTexture;
     }
 
     public void Interact()
--- Assets/Scripts/SaveScripts/SaveController.cs
+++ Assets/Scripts/SaveScripts/SaveController.cs
@@ -1,33 +1,54 @@
-using Assets.Scripts.Models;
-using Assets.Scripts.SaveScripts;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.InputSystem;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 
 public class SaveController : MonoBehaviour
 {
-    private PlayableDirector CurrentPlayableDirector;
-    private StateMashine StateMashine = new StateMashine();
-    public StartSave StartSave { get; private set; } = new StartSave();
+    public PlayableDirector CurrentPlayableDirector { get;set; }
+
+    private CancellationTokenSource CancellationTokenSource;
+    private bool _drawning = false;
+
+    /// <summary>
+    /// Начало сохранения, принимает директора
+    /// </summary>
+    /// <param name="playableDirector"></param>
     public void StartSaves(PlayableDirector playableDirector)
     {
         CurrentPlayableDirector = playableDirector;
         PlayerChangeMap.Instance.ChangeState(PlayerState.Save);
-        StateMashine.ChangeState(StartSave);
         CurrentPlayableDirector.Play();
     }
+
     public void ExitSaves()
     {
         Debug.Log("Выходим из сохранения");
-        CurrentPlayableDirector.Stop();
+        CurrentPlayableDirector?.Stop();
         PlayerChangeMap.Instance.ChangeState(PlayerState.Game);
-        CurrentPlayableDirector = null;
+        //CurrentPlayableDirector = null;
     }
+
     public void ExitSaves(InputAction.CallbackContext callbackContext)
     {
         if (callbackContext.performed)
         {
             ExitSaves();
         }
+    }
+
+    public async void Drawning(InputAction.CallbackContext callbackContext)
+    {
+        _drawning = callbackContext.ReadValueAsButton();
+        Debug.Log(_drawning);
+
+        while (_drawning)
+        {
+            Debug.Log("Я рисую");
+            await UniTask.Yield();
+        }
+
+        ExitSaves();
     }
 }
